5812750f5f229854e5c7675bcdf53281cdbbd6ac
Caio-Ribeiro3 <kaio03693690@gmail.com>
2021-11-10 17:35:31 -0300

    relocate constants file

--- src/features/mapview/api/mapview.ts
+++ src/features/mapview/api/mapview.ts
@@ -1,5 +1,5 @@
+import { BASE_URL } from "../../../constants";
 import { GeoType } from "../types";
-import { BASE_URL } from "./constants";
 
 export const getMapViewData = async (): Promise<{ features: GeoType[] }> => {
   const result = await fetch(`${BASE_URL}/mapview`);
